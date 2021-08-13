55f1d025721626a9c195b46fd51ed9ec28e7ab4e
Jérôme Gurhem <jgurhem@aneo.fr>
2021-08-13 16:07:53 +0000

    give default values to missing configs

--- source/client/csharp/api-v0.1/GridConfig.cs
+++ source/client/csharp/api-v0.1/GridConfig.cs
@@ -18,8 +18,16 @@ namespace HTCGrid
             this.api_gateway_key = root.GetProperty("api_gateway_key").GetString();
             this.redis_with_ssl = root.GetProperty("redis_with_ssl").GetString().ToLower();
             this.redis_port = root.GetProperty("redis_port").GetString();
-            this.redis_ca_cert = root.GetProperty("redis_ca_cert").GetString();
-            this.redis_client_pfx = root.GetProperty("redis_client_pfx").GetString();
+            try {
+                this.redis_ca_cert = root.GetProperty("redis_ca_cert").GetString();
+            } catch (System.Collections.Generic.KeyNotFoundException) {
+                this.redis_ca_cert = "";
+            }
+            try {
+                this.redis_client_pfx = root.GetProperty("redis_client_pfx").GetString();
+            } catch (System.Collections.Generic.KeyNotFoundException) {
+                this.redis_client_pfx = "";
+            }
 			this.connection_redis_timeout = root.GetProperty("connection_redis_timeout").GetString();
             this.cluster_config = root.GetProperty("cluster_config").GetString();
         }
