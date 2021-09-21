efc645daa1efddc90f1d697fe3e9199191a52c27
Lilia ZIANE KHODJA <lzianekhodja@aneo.fr>
2021-09-21 17:46:27 +0200

    Get url endpoint of services on WSL

---
+++ local_deployment/grid/terraform/control_plane/local-values.tf
@@ -0,0 +1,7 @@
+locals {
+  redis_pod_ip = kubernetes_service.redis.status.0.load_balancer.0.ingress.0.ip == "" ? lookup(tomap(data.external.external_ip.result), "external_ip", "localhost") : kubernetes_service.redis.status.0.load_balancer.0.ingress.0.ip
+  redis_without_ssl_pod_ip = kubernetes_service.redis-without-ssl.status.0.load_balancer.0.ingress.0.ip == "" ? lookup(tomap(data.external.external_ip.result), "external_ip", "localhost") : kubernetes_service.redis-without-ssl.status.0.load_balancer.0.ingress.0.ip
+  dynamodb_pod_ip = kubernetes_service.dynamodb.status.0.load_balancer.0.ingress.0.ip == "" ? lookup(tomap(data.external.external_ip.result), "external_ip", "localhost") : kubernetes_service.dynamodb.status.0.load_balancer.0.ingress.0.ip
+  local_services_pod_ip = kubernetes_service.local_services.status.0.load_balancer.0.ingress.0.ip == "" ? lookup(tomap(data.external.external_ip.result), "external_ip", "localhost") : kubernetes_service.local_services.status.0.load_balancer.0.ingress.0.ip
+  dynamodb_table_id = aws_dynamodb_table.htc_tasks_status_table.id
+}
