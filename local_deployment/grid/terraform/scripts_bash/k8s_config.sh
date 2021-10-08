fa4cb869d9aaf10f435664523e30a5250c2e0758
Florian Lemaitre <flemaitre@aneo.fr>
2021-10-08 16:37:19 +0200

    Support For Systemd-Genie

--- local_deployment/grid/terraform/scripts_bash/k8s_config.sh
+++ local_deployment/grid/terraform/scripts_bash/k8s_config.sh
@@ -1,27 +1,38 @@
-#!/bin/bash
+#! /bin/bash
 # Delete existing config
-sed -i '/k8s_config_context/d' parameters.auto.tfvars
-sed -i '/k8s_config_path/d' parameters.auto.tfvars
+sed -i '/k8s_config_\(context\|path\)/d' parameters.auto.tfvars
 
 # Check the OS
-unameOut=$(uname -a)
+unameOut="$(uname -a)"
 case "${unameOut}" in
     *Microsoft*)     OS="WSL";; #must be first since Windows subsystem for linux will have Linux in the name too
-    *microsoft*)     OS="WSL2";; #WARNING: My v2 uses ubuntu 20.4 at the moment slightly different name may not always work
+    *microsoft*) # inside WSL2 (WARNING: My v2 uses ubuntu 20.4 at the moment slightly different name may not always work)
+        if [ "$INSIDE_GENIE" = true ]; then
+            OS="Linux"
+        else
+            OS="WSL2"
+        fi
+        ;;
     Linux*)     OS="Linux";;
     Darwin*)    OS="Mac";;
     *)          OS="UNKNOWN:${unameOut}"
 esac
 
-# Set the configuration
-if [[ "$OS" = ^WSL ]]; then
-    echo " You need to Update to WSL2 in Windows"
-elif [[ "$OS" =~ ^WSL2 ]]; then
-    echo "
-k8s_config_context = \"docker-desktop\"
-k8s_config_path = \"~/.kube/config\""  >> parameters.auto.tfvars
-elif [[ "$OS" =~ ^Linux ]]; then
-    echo "
-k8s_config_context = \"default\"
-k8s_config_path = \"/etc/rancher/k3s/k3s.yaml\""  >> parameters.auto.tfvars
-fi
+case "$OS" in
+    WSL)
+        echo "Error: You need to update to WSL2 in Windows" >&2
+        exit 1
+        ;;
+    WSL2)
+        {
+            echo 'k8s_config_context = "docker-desktop"'
+            echo 'k8s_config_path = "~/.kube/config"'
+        } >> parameters.auto.tfvars
+        ;;
+    Linux)
+        {
+            echo 'k8s_config_context = "default"'
+            echo 'k8s_config_path = "/etc/rancher/k3s/k3s.yaml"'
+        } >> parameters.auto.tfvars
+        ;;
+esac
