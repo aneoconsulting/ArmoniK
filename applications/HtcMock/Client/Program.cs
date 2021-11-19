fd1d55b0508a6dfe399db403a2513e05b8feff35
Jérôme Gurhem <jgurhem@aneo.fr>
2021-11-19 10:10:59 +0100

    Rewrite parallel function to launch mock

--- applications/HtcMock/Client/Program.cs
+++ applications/HtcMock/Client/Program.cs
@@ -9,7 +9,7 @@ using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Diagnostics;
-
+using System.Linq;
 using HTCGrid;
 using Armonik.Mock;
 using Armonik.sdk;
@@ -18,21 +18,47 @@ using Htc.Mock.Core;
 
 namespace HtcClient
 {
+    public static class HtcMockClientExt
+    {
+        public static void ParallelExec(this Client client, RunConfiguration runConfiguration, string conf, int nRun)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            var tasks = Enumerable.Repeat(runConfiguration, nRun)
+                .Select(configuration => Task.Run(() => client.Start(configuration)))
+                .ToList();
+            Task.WhenAll(tasks).Wait();
+            long elapsedMilliseconds = sw.ElapsedMilliseconds;
+            var stat = new SimpleStats() { _ellapsedTime = elapsedMilliseconds, _conf = conf, _test = "AsyncExec", _nRun = nRun };
+            Logger.Info("JSON Result : " + stat.ToJson());
+        }
+        public static void SeqExec(this Client client, RunConfiguration runConfiguration, string conf, int nRun)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            for (int i = 0; i < nRun; i++)
+            {
+                client.Start(runConfiguration);
+            }
+            long elapsedMilliseconds = sw.ElapsedMilliseconds;
+            var stat = new SimpleStats() { _ellapsedTime = elapsedMilliseconds, _conf = conf, _test = "SeqExec", _nRun = nRun };
+            Logger.Info("JSON Result : " + stat.ToJson());
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Mock!");
-             var armonik_wait_client = Environment.GetEnvironmentVariable("ARMONIK_DEBUG_WAIT_CLIENT");
-             if(!String.IsNullOrEmpty(armonik_wait_client))
-             {
+            var armonik_wait_client = Environment.GetEnvironmentVariable("ARMONIK_DEBUG_WAIT_CLIENT");
+            if (!String.IsNullOrEmpty(armonik_wait_client))
+            {
                 int arminik_debug_wait_client = int.Parse(armonik_wait_client);
 
-             if (arminik_debug_wait_client > 0)
-            {
-                Console.WriteLine($"Debug: Sleep {arminik_debug_wait_client} seconds");
-                Thread.Sleep(arminik_debug_wait_client * 1000);
-            }
+                if (arminik_debug_wait_client > 0)
+                {
+                    Console.WriteLine($"Debug: Sleep {arminik_debug_wait_client} seconds");
+                    Thread.Sleep(arminik_debug_wait_client * 1000);
+                }
             }
             string agentConfigFileName = Environment.GetEnvironmentVariable("AGENT_CONFIG_FILE");
             if (agentConfigFileName == null)
@@ -59,10 +85,10 @@ namespace HtcClient
             gridConfig.Init(parsedConfig);
             //get envirnoment variable
             var var_env = Environment.GetEnvironmentVariable("ARMONIK_DEBUG_WAIT_TASK");
-             if(!String.IsNullOrEmpty(var_env))
-             {
+            if (!String.IsNullOrEmpty(var_env))
+            {
                 gridConfig.debug = int.Parse(var_env);
-             }
+            }
 
 
 
@@ -76,35 +102,8 @@ namespace HtcClient
             // Timespan(heures, minutes, secondes)
             // RunConfiguration runConfiguration = RunConfiguration.XSmall; // result : Aggregate_1871498793_result
             // RunConfiguration runConfiguration = new RunConfiguration(new TimeSpan(0, 0, 0, 0, 100), 10, 1, 1, 1);
-            // AsyncExec(client, RunConfiguration.XSmall, "XSmall", 5);
-            SeqExec(client, RunConfiguration.XSmall, "XSmall", 5);
-        }
-
-        public static async void AsyncExec(Client client, RunConfiguration runConfiguration, string conf, int nRun)
-        {
-            Stopwatch sw = Stopwatch.StartNew();
-            var tasks = new List<Task>();
-            for (int i = 0; i < nRun; i++)
-            {
-                tasks.Add(Task.Run(() => client.Start(runConfiguration)));
-                Thread.Sleep(10000);
-            }
-            Task.WaitAll(tasks.ToArray());
-            long elapsedMilliseconds = sw.ElapsedMilliseconds;
-            var stat = new SimpleStats() { _ellapsedTime = elapsedMilliseconds, _conf = conf, _test = "AsyncExec", _nRun = nRun };
-            Logger.Info("JSON Result : " + stat.ToJson());
-        }
-
-        public static void SeqExec(Client client, RunConfiguration runConfiguration, string conf, int nRun)
-        {
-            Stopwatch sw = Stopwatch.StartNew();
-            for (int i = 0; i < nRun; i++)
-            {
-                client.Start(runConfiguration);
-            }
-            long elapsedMilliseconds = sw.ElapsedMilliseconds;
-            var stat = new SimpleStats() { _ellapsedTime = elapsedMilliseconds, _conf = conf, _test = "SeqExec", _nRun = nRun };
-            Logger.Info("JSON Result : " + stat.ToJson());
+            client.ParallelExec(RunConfiguration.XSmall, "XSmall", 5);
+            client.SeqExec(RunConfiguration.XSmall, "XSmall", 5);
         }
 
     }
