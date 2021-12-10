d77b489772fa1aea92226f417a5108572cb27c85
Jérôme Gurhem <jgurhem@aneo.fr>
2021-12-10 10:29:58 +0000

    update script to build images

--- tools/imgs.py
+++ tools/imgs.py
@@ -1,6 +1,7 @@
 import json
 import os
 import subprocess
+import sys
 
 def findTag(imgs):
     tags = []
@@ -11,7 +12,10 @@ def findTag(imgs):
             tags.append(i['Tag'])
         elif i['Repository'] == 'dockerhubaneo/armonik_pollingagent':
             tags.append(i['Tag'])
-    return sorted(tags).pop()
+    if len(tags) > 0:
+        return sorted(tags).pop()
+    else:
+        return 'dev-1000'
 
 def incrTag(tag):
     s = tag.split('-')
@@ -23,16 +27,26 @@ tag = findTag(imgs)
 tag = incrTag(tag)
 print(tag)
 
-p1 = subprocess.Popen(f'docker build -t dockerhubaneo/armonik_control:{tag} -f src/Control/src/ArmoniK.Control.Service/ArmoniK.Control/Dockerfile .', shell=True, stdout=subprocess.PIPE)
-p2 = subprocess.Popen(f'docker build -t dockerhubaneo/armonik_pollingagent:{tag} -f ./src/Compute/PollingAgent/src/ArmoniK.Compute.PollingAgent/Dockerfile .', shell=True, stdout=subprocess.PIPE)
-p3 = subprocess.Popen(f'docker build -t dockerhubaneo/armonik_compute:{tag} -f ./src/DevelopmentKit/csharp/SymphonyApi/Gridlib/src/ArmoniK.DevelopmentKit.SymphonyApi/Dockerfile .', shell=True, stdout=subprocess.PIPE)
+p1 = subprocess.Popen(f'docker build -t dockerhubaneo/armonik_control:{tag} -f src/Control/src/ArmoniK.Control.Service/ArmoniK.Control/Dockerfile .', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+p2 = subprocess.Popen(f'docker build -t dockerhubaneo/armonik_pollingagent:{tag} -f ./src/Compute/PollingAgent/src/ArmoniK.Compute.PollingAgent/Dockerfile .', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+p3 = subprocess.Popen(f'docker build -t dockerhubaneo/armonik_compute:{tag} -f ./src/DevelopmentKit/csharp/SymphonyApi/Gridlib/src/ArmoniK.DevelopmentKit.SymphonyApi/Dockerfile .', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 print(p1.communicate()[0].decode('utf-8'))
 print(p2.communicate()[0].decode('utf-8'))
 print(p3.communicate()[0].decode('utf-8'))
 
+if p1.returncode or p2.returncode or p3.returncode:
+    print('error while building')
+    if p1.returncode:
+        print(p1.communicate()[1].decode('utf-8'))
+    if p2.returncode:
+        print(p2.communicate()[1].decode('utf-8'))
+    if p3.returncode:
+        print(p3.communicate()[1].decode('utf-8'))
+    sys.exit(1)
+
 os.popen(f'sed s/dev-[0-9]*/{tag}/g -i ../../infrastructure/localhost/deploy/parameters.tfvars')
 
 # kubectl logs -n armonik svc/control-plane
-# kubectl logs -n armonik deployment/compute-plane -c polling-agent
-# kubectl logs -n armonik deployment/compute-plane -c compute
+# kubectl logs -n armonik deployment/compute-plane-0 -c polling-agent
+# kubectl logs -n armonik deployment/compute-plane-0 -c compute-0
