d69c3ccfda7768b7d899af29356bb0fa357c2b6c
Jérôme Gurhem <jgurhem@aneo.fr>
2021-12-15 15:30:51 +0000

    Fix tool script

--- tools/utils/disply_agent_logs.sh
+++ tools/utils/disply_agent_logs.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-for i in $(kubectl get po | grep armonik-agent | cut -d' ' -f1)
+for i in $(kubectl get po -n armonik | grep ^compute-plane- | cut -d' ' -f1)
 do
 echo
 echo -------------------------------------------------------------
 echo $i
 echo -------------------------------------------------------------
 echo -------------------------------------------------------------
-kubectl logs $i -c lambda | grep Empty | wc -l
+#kubectl logs $i -n armonik -c compute-0 | grep -E "Htc.Mock|Execute"
+kubectl logs $i -n armonik -c compute-0 --tail=30
+echo -------------------------------------------------------------
+echo -------------------------------------------------------------
+kubectl logs $i -n armonik -c polling-agent --tail=30
 echo -------------------------------------------------------------
 echo -------------------------------------------------------------
 echo
