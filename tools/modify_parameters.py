3c6589a75ffcaa6e0c1902064c85c4915c624084
Jérôme Gurhem <jgurhem@aneo.fr>
2021-12-21 16:32:51 +0000

    Remake pipelines

--- tools/modify_parameters.py
+++ tools/modify_parameters.py
@@ -7,8 +7,8 @@ parser.add_argument("inputfile", help="Path to the input paramters.tfvars file",
 parser.add_argument("outputfile", help="Path to the output paramters.tfvars.json file", type=str)
 parser.add_argument("--control-tag", dest="controltag", help="Tag for control plane image", type=str, default=None)
 parser.add_argument("--control-img", dest="controlimg", help="registry/img for control plane image", type=str, default=None)
-parser.add_argument("--compute-tag", dest="computetag", help="Tag for compute plane image", type=str, default=None)
-parser.add_argument("--compute-img", dest="computeimg", help="registry/img for compute plane image", type=str, default=None)
+parser.add_argument("--worker-tag", dest="workertag", help="Tag for compute plane worker image", type=str, default=None)
+parser.add_argument("--worker-img", dest="workerimg", help="registry/img for compute plane worker image", type=str, default=None)
 parser.add_argument("--agent-tag", dest="agenttag", help="Tag for polling agent image", type=str, default=None)
 parser.add_argument("--agent-img", dest="agentimg", help="registry/img for polling agent image", type=str, default=None)
 args = parser.parse_args()
@@ -21,16 +21,19 @@ if args.controlimg != None:
 if args.controltag != None:
     content['armonik']['control_plane']['tag'] = args.controltag
 
-if args.computeimg != None:
-    content['armonik']['compute_plane']['polling_agent']['image']
-if args.computetag != None:
-    content['armonik']['compute_plane']['polling_agent']['tag']
+if args.agentimg != None:
+    content['armonik']['compute_plane']['polling_agent']['image'] = args.agentimg
+if args.agenttag != None:
+    content['armonik']['compute_plane']['polling_agent']['tag'] = args.agenttag
 
-for c in content['armonik']['compute_plane']['compute']:
-    if args.agenttag != None:
-        c['tag'] = args.agenttag
-    if args.agentimg != None:
-        c['image'] = args.agentimg
+if args.workertag != None or args.workerimg != None:
+    workers = content['armonik']['compute_plane']['compute']
+    for w in workers:
+        if args.workertag != None:
+            w['tag'] = args.workertag
+        if args.workerimg != None:
+            w['image'] = args.workerimg
+    content['armonik']['compute_plane']['compute'] = workers
 
 with open(args.outputfile, 'w') as fout:
     json.dump(content, fout, indent=2, sort_keys=True)
