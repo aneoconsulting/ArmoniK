f454707077742cade3b669ccc3bb3281698e5107
Lilia ZIANE KHODJA <lzianekhodja@aneo.fr>
2022-01-04 15:04:54 +0100

    Add test for symphony like

---
+++ tools/tests/symphony_like.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd ../../source/ArmoniK.Samples
+git submodule update --init
+
+export CPIP=$(kubectl get svc control-plane -n armonik -o custom-columns="IP:.status.loadBalancer.ingress[*].ip" --no-headers=true)
+export CPPort=$(kubectl get svc control-plane -n armonik -o custom-columns="PORT:.spec.ports[*].port" --no-headers=true)
+export Grpc__Endpoint=http://$CPIP:$CPPort
+
+cd Samples/SymphonyLike/
+dotnet publish --self-contained -r linux-x64 SymphonyLike.sln
+
+sudo mkdir -p /data
+sudo chown -R $USER:$USER /data
+cp packages/ArmoniK.Samples.SymphonyPackage-v1.0.0.zip /data
+
+cd ArmoniK.Samples.SymphonyClient/
+dotnet bin/Debug/net5.0/linux-x64/ArmoniK.Samples.SymphonyClient.dll
