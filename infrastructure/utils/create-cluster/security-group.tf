365928bc34251420a79b3b0fa6ec37cc9ecdb6d4
Lilia ZIANE KHODJA <lzianekhodja@aneo.fr>
2022-01-11 10:20:25 +0100

    Open port 8080 in security group in the cluster

--- infrastructure/utils/create-cluster/security-group.tf
+++ infrastructure/utils/create-cluster/security-group.tf
@@ -103,5 +103,13 @@ resource "aws_security_group" "services_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    description = "Seq"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
---
+++ tools/tests/symphony_like_cluster.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd ../../source/ArmoniK.Samples
+git submodule update --init
+git pull origin main
+git checkout main
+
+export Grpc__Endpoint="http://52.37.128.106:5001"
+
+cd Samples/SymphonyLike/
+dotnet publish --self-contained -r linux-x64 SymphonyLike.sln
+
+scp -i ~/.ssh/cluster-key packages/ArmoniK.Samples.SymphonyPackage-v1.0.0.zip ec2-user@35.88.236.37:/data
+
+cd ArmoniK.Samples.SymphonyClient/
+dotnet bin/Debug/net5.0/linux-x64/ArmoniK.Samples.SymphonyClient.dll
