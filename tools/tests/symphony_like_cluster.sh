4cbb04859221d27000b166491554bf5d0e3c81c5
Lilia ZIANE KHODJA <lzianekhodja@aneo.fr>
2022-01-13 08:29:44 +0100

    Update symphony like test for onpremise cluster

--- tools/tests/symphony_like_cluster.sh
+++ tools/tests/symphony_like_cluster.sh
@@ -5,12 +5,12 @@ git submodule update --init
 git pull origin main
 git checkout main
 
-export Grpc__Endpoint="http://34.222.110.169:5001"
+export Grpc__Endpoint="http://54.184.12.189:30001"
 
 cd Samples/SymphonyLike/
 dotnet publish --self-contained -r linux-x64 SymphonyLike.sln
 
-scp -i ~/.ssh/cluster-key packages/ArmoniK.Samples.SymphonyPackage-v1.0.0.zip ec2-user@35.88.236.37:/data
+scp -i ~/.ssh/cluster-key packages/ArmoniK.Samples.SymphonyPackage-v1.0.0.zip ec2-user@34.222.5.66:/data
 
 cd ArmoniK.Samples.SymphonyClient/
 dotnet bin/Debug/net5.0/linux-x64/ArmoniK.Samples.SymphonyClient.dll
