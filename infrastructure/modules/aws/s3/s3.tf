16f92001d654d7ff0bfa265e541ef039499165cc
Lilia ZIANE KHODJA <lzianekhodja@aneo.fr>
2022-02-09 10:15:26 +0100

    set aws_s3_bucket_public_access_block=false

--- infrastructure/modules/aws/s3/s3.tf
+++ infrastructure/modules/aws/s3/s3.tf
@@ -10,6 +10,11 @@ module "s3_bucket" {
   tags                                  = local.tags
   attach_deny_insecure_transport_policy = true
   attach_require_latest_tls_policy      = true
+  attach_public_policy                  = false
+  block_public_acls                     = true
+  block_public_policy                   = true
+  ignore_public_acls                    = true
+  restrict_public_buckets               = true
   server_side_encryption_configuration  = {
     rule = {
       apply_server_side_encryption_by_default = {
