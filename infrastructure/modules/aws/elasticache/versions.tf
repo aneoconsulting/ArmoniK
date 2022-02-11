97e74aedc7161c3ec7369b6ff9cf825523b156e0
Lilia ZIANE KHODJA <lzianekhodja@aneo.fr>
2022-02-11 10:27:56 +0100

    parralelism =1 for ecr

---
+++ infrastructure/modules/aws/elasticache/versions.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws    = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0.0"
+    }
+    null   = {
+      source  = "hashicorp/null"
+      version = "~> 3.1.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1.0"
+    }
+  }
+}
---
+++ infrastructure/modules/aws/vpc/versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws    = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1.0"
+    }
+  }
+}
