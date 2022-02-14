1f12791336655877a11e3c3e4cfd459ce55e3dff
Lilia ZIANE KHODJA <lzianekhodja@aneo.fr>
2022-02-14 15:22:16 +0100

    Delete default values for armonik

--- infrastructure/armonik/variables.tf
+++ infrastructure/armonik/variables.tf
@@ -219,22 +219,6 @@ variable "control_plane" {
     })
     image_pull_secrets = string
   })
-  default     = {
-    replicas           = 1
-    image              = "dockerhubaneo/armonik_control"
-    tag                = "0.0.4"
-    image_pull_policy  = "IfNotPresent"
-    port               = 5001
-    limits             = {
-      cpu    = "1000m"
-      memory = "1024Mi"
-    }
-    requests           = {
-      cpu    = "100m"
-      memory = "128Mi"
-    }
-    image_pull_secrets = ""
-  }
 }
 
 # Parameters of the compute plane
@@ -275,46 +259,6 @@ variable "compute_plane" {
       })
     }))
   })
-  default     = {
-    # number of replicas for each deployment of compute plane
-    replicas                         = 1
-    termination_grace_period_seconds = 30
-    # number of queues according to priority of tasks
-    max_priority                     = 1
-    image_pull_secrets               = ""
-    # ArmoniK polling agent
-    polling_agent                    = {
-      image             = "dockerhubaneo/armonik_pollingagent"
-      tag               = "0.0.4"
-      image_pull_policy = "IfNotPresent"
-      limits            = {
-        cpu    = "100m"
-        memory = "128Mi"
-      }
-      requests          = {
-        cpu    = "100m"
-        memory = "128Mi"
-      }
-    }
-    # ArmoniK workers
-    worker                           = [
-      {
-        name              = "worker"
-        port              = 80
-        image             = "dockerhubaneo/armonik_worker_dll"
-        tag               = "0.0.4"
-        image_pull_policy = "IfNotPresent"
-        limits            = {
-          cpu    = "920m"
-          memory = "2048Mi"
-        }
-        requests          = {
-          cpu    = "50m"
-          memory = "100Mi"
-        }
-      }
-    ]
-  }
 }
 
 # Working dir
