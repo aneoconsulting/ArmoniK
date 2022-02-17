41dee0432f50ecff8929cf23636b7659b574b05b
Lilia ZIANE KHODJA <lzianekhodja@aneo.fr>
2022-02-17 11:10:32 +0100

    Fix symphony_endToendTests.sh test

--- tools/tests/symphony_endToendTests.sh
+++ tools/tests/symphony_endToendTests.sh
@@ -2,7 +2,7 @@
 
 BASEDIR=$(dirname "$0")
 pushd $BASEDIR
-  BASEDIR=$(pwd -P)
+BASEDIR=$(pwd -P)
 popd
 
 export MODE=""
@@ -14,64 +14,54 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 DRY_RUN="${DRY_RUN:-0}"
 
-pushd $(dirname $0) > /dev/null 2>&1
+pushd $(dirname $0) >/dev/null 2>&1
 BASEDIR=$(pwd -P)
-popd > /dev/null 2>&1
+popd >/dev/null 2>&1
 
 configuration=Debug
 
-TestDir=${BASEDIR}/../../source/ArmoniK.Extensions.Csharp/SymphonyApi/ArmoniK.DevelopmentKit.SymphonyApi.Tests/EndToEnd.Tests/
+TestDir=${BASEDIR}/../../source/ArmoniK.Extensions.Csharp/SymphonyApi/ArmoniK.DevelopmentKit.SymphonyApi.Tests/EndToEnd.Tests
 cd ${TestDir}
-
+echo ${TestDir}
 export CPIP=$(kubectl get svc control-plane -n armonik -o custom-columns="IP:.spec.clusterIP" --no-headers=true)
 export CPPort=$(kubectl get svc control-plane -n armonik -o custom-columns="PORT:.spec.ports[*].port" --no-headers=true)
 export Grpc__Endpoint=http://$CPIP:$CPPort
 nuget_cache=$(dotnet nuget locals global-packages --list | awk '{ print $2 }')
 
-function build()
-{
-    cd ${TestDir}
-    echo rm -rf ${nuget_cache}/armonik.*
-    rm -rf $(dotnet nuget locals global-packages --list | awk '{ print $2 }')/armonik.*
-
-    dotnet publish --self-contained -c $configuration -r linux-x64 -f net5.0 .
+function build() {
+  cd ${TestDir}
+  echo rm -rf ${nuget_cache}/armonik.*
+  rm -rf $(dotnet nuget locals global-packages --list | awk '{ print $2 }')/armonik.*
+  dotnet publish --self-contained -c $configuration -r linux-x64 -f net5.0 .
 }
 
-function deploy()
-{
-    if [ ! -d "/data" ]; then
-      sudo mkdir -p /data
-    fi
-    if [ ! -w "/data" ]; then
-      sudo chown -R $USER:$USER /data
-    fi
-
-    cp -v ../packages/ArmoniK.EndToEndTests-v1.0.0.zip /data
-    kubectl delete -n armonik $(kubectl get pods -n armonik -l service=compute-plane --no-headers=true -o name)
-
+function deploy() {
+  if [ ! -d "/data" ]; then
+    sudo mkdir -p /data
+  fi
+  if [ ! -w "/data" ]; then
+    sudo chown -R $USER:$USER /data
+  fi
+  cp -v ../packages/ArmoniK.EndToEndTests-v1.0.0.zip /data
 }
 
-function execute()
-{
-    cd ${TestDir}
-
-    if [ ! -d "/data" ]; then
-      sudo mkdir -p /data
-    fi
-    if [ ! -w "/data" ]; then
-      sudo chown -R $USER:$USER /data
-    fi
-
-    cp -v ../packages/ArmoniK.EndToEndTests-v1.0.0.zip /data
-
-    #kubectl delete -n armonik $(kubectl get pods -n armonik -l service=compute-plane --no-headers=true -o name)
-    dotnet bin/${configuration}/net5.0/linux-x64/ArmoniK.EndToEndTests.dll
+function execute() {
+  cd ${TestDir}
+  if [ ! -d "/data" ]; then
+    sudo mkdir -p /data
+  fi
+  if [ ! -w "/data" ]; then
+    sudo chown -R $USER:$USER /data
+  fi
+  cp -v ../packages/ArmoniK.EndToEndTests-v1.0.0.zip /data
+  kubectl delete -n armonik $(kubectl get pods -n armonik -l service=compute-plane --no-headers=true -o name)
+  dotnet bin/${configuration}/net5.0/linux-x64/ArmoniK.EndToEndTests.dll
 }
 
 function usage() {
   echo "Usage: $0 [option...]  with : " >&2
   echo
-cat <<- EOF
+  cat <<-EOF
         no option           : To build and Run tests
         -b | --build        : To build only test and package
         -r | --run          : To run only deploy package and test
@@ -81,20 +71,18 @@ EOF
   exit 0
 }
 
-
 DEFAULT=FALSE
 MODE=All
-function main()
-{
-    echo "Nb Arguments : $#"
-    if [[ $# == 0 ]]; then
-        build
-	deploy
-        execute
-        exit 0
-    fi
+function main() {
+  echo "Nb Arguments : $#"
+  if [[ $# == 0 ]]; then
+    build
+    deploy
+    execute
+    exit 0
+  fi
 
-    for i in "$@"; do
+  for i in "$@"; do
     case $i in
     -h | --help)
       usage
@@ -109,7 +97,6 @@ function main()
       build
       shift # past argument=value
       ;;
-
     -a | *)
       # unknown option
       build
