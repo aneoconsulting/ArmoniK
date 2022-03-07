15c63233c42db6106ba08be4a6249fc0d23118fd
Lilia ZIANE KHODJA <lzianekhodja@aneo.fr>
2022-03-07 10:37:49 +0100

    Add sts vpc endpoints

--- infrastructure/modules/aws/vpc/endpoints.tf
+++ infrastructure/modules/aws/vpc/endpoints.tf
@@ -9,57 +9,69 @@ module "vpc_endpoints" {
   ]
 
   endpoints = {
-    s3              = {
+    s3                   = {
       service         = "s3"
       service_type    = "Gateway"
       route_table_ids = flatten([
         module.vpc.intra_route_table_ids, module.vpc.private_route_table_ids, module.vpc.public_route_table_ids
       ])
     }
-    ec2_autoscaling = {
+    ec2_autoscaling      = {
       service             = "autoscaling"
       private_dns_enabled = var.vpc.enable_private_subnet
       subnet_ids          = var.vpc.enable_private_subnet ? matchkeys(module.vpc.private_subnets, tolist(module.vpc.private_subnets_cidr_blocks), var.vpc.private_subnets) : []
       security_group_ids  = var.vpc.enable_private_subnet ? [module.vpc.default_security_group_id] : []
     }
-    ec2             = {
+    ec2                  = {
       service             = "ec2"
       private_dns_enabled = var.vpc.enable_private_subnet
       subnet_ids          = var.vpc.enable_private_subnet ? matchkeys(module.vpc.private_subnets, tolist(module.vpc.private_subnets_cidr_blocks), var.vpc.private_subnets) : []
       security_group_ids  = var.vpc.enable_private_subnet ? [module.vpc.default_security_group_id] : []
     }
-    ecr_dkr         = {
+    ecr_dkr              = {
       service             = "ecr.dkr"
       private_dns_enabled = var.vpc.enable_private_subnet
       subnet_ids          = var.vpc.enable_private_subnet ? matchkeys(module.vpc.private_subnets, tolist(module.vpc.private_subnets_cidr_blocks), var.vpc.private_subnets) : []
       security_group_ids  = var.vpc.enable_private_subnet ? [module.vpc.default_security_group_id] : []
     }
-    ecr_api         = {
+    ecr_api              = {
       service             = "ecr.api"
       private_dns_enabled = var.vpc.enable_private_subnet
       subnet_ids          = var.vpc.enable_private_subnet ? matchkeys(module.vpc.private_subnets, tolist(module.vpc.private_subnets_cidr_blocks), var.vpc.private_subnets) : []
       security_group_ids  = var.vpc.enable_private_subnet ? [module.vpc.default_security_group_id] : []
     }
-    logs            = {
+    logs                 = {
       service             = "logs"
       private_dns_enabled = var.vpc.enable_private_subnet
       subnet_ids          = var.vpc.enable_private_subnet ? matchkeys(module.vpc.private_subnets, tolist(module.vpc.private_subnets_cidr_blocks), var.vpc.private_subnets) : []
       security_group_ids  = var.vpc.enable_private_subnet ? [module.vpc.default_security_group_id] : []
     }
-    ssm             = {
+    sts                  = {
+      service             = "sts"
+      private_dns_enabled = var.vpc.enable_private_subnet
+      subnet_ids          = var.vpc.enable_private_subnet ? matchkeys(module.vpc.private_subnets, tolist(module.vpc.private_subnets_cidr_blocks), var.vpc.private_subnets) : []
+      security_group_ids  = var.vpc.enable_private_subnet ? [module.vpc.default_security_group_id] : []
+    }
+    ssm                  = {
       service             = "ssm"
       private_dns_enabled = var.vpc.enable_private_subnet
       subnet_ids          = var.vpc.enable_private_subnet ? matchkeys(module.vpc.private_subnets, tolist(module.vpc.private_subnets_cidr_blocks), var.vpc.private_subnets) : []
       security_group_ids  = var.vpc.enable_private_subnet ? [module.vpc.default_security_group_id] : []
     }
-    ssmmessages     = {
+    ssmmessages          = {
       service             = "ssmmessages"
       private_dns_enabled = var.vpc.enable_private_subnet
       subnet_ids          = var.vpc.enable_private_subnet ? matchkeys(module.vpc.private_subnets, tolist(module.vpc.private_subnets_cidr_blocks), var.vpc.private_subnets) : []
       security_group_ids  = var.vpc.enable_private_subnet ? [module.vpc.default_security_group_id] : []
     }
-    /*sqs                  = {
-      service             = "sqs"
+    elasticloadbalancing = {
+      service             = "elasticloadbalancing"
+      private_dns_enabled = var.vpc.enable_private_subnet
+      subnet_ids          = var.vpc.enable_private_subnet ? matchkeys(module.vpc.private_subnets, tolist(module.vpc.private_subnets_cidr_blocks), var.vpc.private_subnets) : []
+      security_group_ids  = var.vpc.enable_private_subnet ? [module.vpc.default_security_group_id] : []
+    }
+    monitoring           = {
+      service             = "monitoring"
       private_dns_enabled = var.vpc.enable_private_subnet
       subnet_ids          = var.vpc.enable_private_subnet ? matchkeys(module.vpc.private_subnets, tolist(module.vpc.private_subnets_cidr_blocks), var.vpc.private_subnets) : []
       security_group_ids  = var.vpc.enable_private_subnet ? [module.vpc.default_security_group_id] : []
@@ -71,14 +83,8 @@ module "vpc_endpoints" {
         module.vpc.intra_route_table_ids, module.vpc.private_route_table_ids, module.vpc.public_route_table_ids
       ])
     }
-    monitoring           = {
-      service             = "monitoring"
-      private_dns_enabled = var.vpc.enable_private_subnet
-      subnet_ids          = var.vpc.enable_private_subnet ? matchkeys(module.vpc.private_subnets, tolist(module.vpc.private_subnets_cidr_blocks), var.vpc.private_subnets) : []
-      security_group_ids  = var.vpc.enable_private_subnet ? [module.vpc.default_security_group_id] : []
-    }
-    elasticloadbalancing = {
-      service             = "elasticloadbalancing"
+    /*sqs                  = {
+      service             = "sqs"
       private_dns_enabled = var.vpc.enable_private_subnet
       subnet_ids          = var.vpc.enable_private_subnet ? matchkeys(module.vpc.private_subnets, tolist(module.vpc.private_subnets_cidr_blocks), var.vpc.private_subnets) : []
       security_group_ids  = var.vpc.enable_private_subnet ? [module.vpc.default_security_group_id] : []
