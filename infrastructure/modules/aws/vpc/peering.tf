708fe39f5ae82ea6eec60453099c9054f994d822
Lilia ZIANE KHODJA <lzianekhodja@aneo.fr>
2022-03-07 14:30:52 +0100

    Update peering

--- infrastructure/modules/aws/vpc/peering.tf
+++ infrastructure/modules/aws/vpc/peering.tf
@@ -3,14 +3,6 @@ resource "aws_vpc_peering_connection" "peering" {
   peer_owner_id = data.aws_caller_identity.current.id
   peer_vpc_id   = var.vpc.peering.peer_vpc_ids[count.index]
   vpc_id        = module.vpc.vpc_id
-  accepter {
-    allow_classic_link_to_remote_vpc = true
-    allow_vpc_to_remote_classic_link = true
-  }
-  requester {
-    allow_classic_link_to_remote_vpc = true
-    allow_vpc_to_remote_classic_link = true
-  }
   auto_accept   = true
   tags          = local.tags
   depends_on    = [module.vpc]
