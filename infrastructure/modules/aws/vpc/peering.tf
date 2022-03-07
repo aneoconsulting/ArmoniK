5eaff27ddc76457e8d2407ea8d909b9b84d35bda
Lilia ZIANE KHODJA <lzianekhodja@aneo.fr>
2022-03-07 10:44:46 +0100

    Fix peering in vpc module

--- infrastructure/modules/aws/vpc/peering.tf
+++ infrastructure/modules/aws/vpc/peering.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc_peering_connection" "peering" {
   count         = (var.vpc.peering.enabled ? length(var.vpc.peering.peer_vpc_ids) : 0)
   peer_owner_id = data.aws_caller_identity.current.id
-  peer_vpc_id   = var.vpc.peering.peering.peer_vpc_ids[count.index]
+  peer_vpc_id   = var.vpc.peering.peer_vpc_ids[count.index]
   vpc_id        = module.vpc.vpc_id
   accepter {
     allow_remote_vpc_dns_resolution = true
