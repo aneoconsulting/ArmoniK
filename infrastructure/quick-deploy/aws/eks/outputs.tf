ff8ea21a3a1ec27333f8ee6794175db621f71f2e
Lilia ZIANE KHODJA <lzianekhodja@aneo.fr>
2022-03-08 15:14:37 +0100

    Update the path of the kubeconfig in output

--- infrastructure/quick-deploy/aws/eks/outputs.tf
+++ infrastructure/quick-deploy/aws/eks/outputs.tf
@@ -5,6 +5,6 @@ output "eks" {
     name                    = module.eks.name
     cluster_id              = module.eks.cluster_id
     worker_iam_role_name    = module.eks.worker_iam_role_name
-    kubectl_config_filename = module.eks.kubectl_config_filename
+    kubectl_config_filename = abspath(module.eks.kubectl_config_filename)
   }
 }
