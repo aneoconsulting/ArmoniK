a599fdc707f7db517c9041ec33ab1b219f521744
Lilia ZIANE KHODJA <lzianekhodja@aneo.fr>
2022-04-05 16:59:17 +0200

    Add port for Kubernetes dashboard on the client

--- infrastructure/docs/client-node/security-group.tf
+++ infrastructure/docs/client-node/security-group.tf
@@ -28,6 +28,13 @@ resource "aws_security_group" "client" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "Dashboard"
+    from_port   = 8001
+    to_port     = 8001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
