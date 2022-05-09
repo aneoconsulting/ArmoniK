99bded4fa0343be257b783eacc17ba0bf41ce4e2
Lilia ZIANE KHODJA <lzianekhodja@aneo.fr>
2022-05-09 18:33:34 +0200

    Add mongodb_polling_delay in armonik

--- infrastructure/quick-deploy/aws/armonik/main.tf
+++ infrastructure/quick-deploy/aws/armonik/main.tf
@@ -1,11 +1,12 @@
 module "armonik" {
-  source               = "../../../modules/armonik"
-  working_dir          = "${path.root}/../../.."
-  namespace            = var.namespace
-  logging_level        = var.logging_level
-  storage_endpoint_url = var.storage_endpoint_url
-  monitoring           = var.monitoring
-  compute_plane        = var.compute_plane
-  control_plane        = var.control_plane
-  ingress              = var.ingress
+  source                = "../../../modules/armonik"
+  working_dir           = "${path.root}/../../.."
+  namespace             = var.namespace
+  logging_level         = var.logging_level
+  mongodb_polling_delay = var.mongodb_polling_delay
+  storage_endpoint_url  = var.storage_endpoint_url
+  monitoring            = var.monitoring
+  compute_plane         = var.compute_plane
+  control_plane         = var.control_plane
+  ingress               = var.ingress
 }
