46035b891322cf4d4e2c760425cfd020b082c2f6
Lilia ZIANE KHODJA <lzianekhodja@aneo.fr>
2022-05-11 00:38:17 +0200

    Ingress http port to 5000

--- infrastructure/docs/client-node/security-group.tf
+++ infrastructure/docs/client-node/security-group.tf
@@ -41,6 +41,13 @@ resource "aws_security_group" "client" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "ingress"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
