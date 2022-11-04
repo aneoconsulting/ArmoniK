8255295b6a39b54cc7ee6d852c6052d69747eaaf
lzianekhodja <lzianekhodja@aneo.fr>
2022-11-04 12:24:25 +0100

    Rename the file S3 iam in storage

--- infrastructure/quick-deploy/aws/storage/s3-iam.tf
+++ infrastructure/quick-deploy/aws/storage/s3-iam.tf
@@ -1,7 +1,7 @@
 # Decrypt objects in S3
-data "aws_iam_policy_document" "decrypt_object_document" {
+data "aws_iam_policy_document" "decrypt_object" {
   statement {
-    sid = "KMSAccess"
+    sid     = "KMSAccess"
     actions = [
       "kms:Encrypt",
       "kms:Decrypt",
@@ -9,47 +9,47 @@ data "aws_iam_policy_document" "decrypt_object_document" {
       "kms:GenerateDataKey*",
       "kms:DescribeKey"
     ]
-    effect = "Allow"
+    effect    = "Allow"
     resources = [
       local.s3_fs_kms_key_id
     ]
   }
 }
 
-resource "aws_iam_policy" "decrypt_object_policy" {
+resource "aws_iam_policy" "decrypt_object" {
   name_prefix = local.iam_s3_decrypt_object_policy_name
   description = "Policy for alowing decryption of encrypted object in S3 ${var.eks.cluster_id}"
-  policy      = data.aws_iam_policy_document.decrypt_object_document.json
+  policy      = data.aws_iam_policy_document.decrypt_object.json
   tags        = local.tags
 }
 
-resource "aws_iam_role_policy_attachment" "decrypt_object_attachment" {
-  policy_arn = aws_iam_policy.decrypt_object_policy.arn
+resource "aws_iam_role_policy_attachment" "decrypt_object" {
+  policy_arn = aws_iam_policy.decrypt_object.arn
   role       = var.eks.worker_iam_role_name
 }
 
 # Read objects in S3
-data "aws_iam_policy_document" "read_object_document" {
+data "aws_iam_policy_document" "read_object" {
   statement {
-    sid = "ReadFromS3"
+    sid     = "ReadFromS3"
     actions = [
       "s3:GetObject"
     ]
-    effect = "Allow"
+    effect    = "Allow"
     resources = [
       "${module.s3_fs.arn}/*"
     ]
   }
 }
 
-resource "aws_iam_policy" "read_object_policy" {
+resource "aws_iam_policy" "read_object" {
   name_prefix = "s3-read-${var.eks.cluster_id}"
   description = "Policy for allowing read object in S3 ${var.eks.cluster_id}"
-  policy      = data.aws_iam_policy_document.read_object_document.json
+  policy      = data.aws_iam_policy_document.read_object.json
   tags        = local.tags
 }
 
 resource "aws_iam_role_policy_attachment" "read_object_attachment" {
-  policy_arn = aws_iam_policy.read_object_policy.arn
+  policy_arn = aws_iam_policy.read_object.arn
   role       = var.eks.worker_iam_role_name
 }
