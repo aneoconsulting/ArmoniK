bcdcbf5980122b57ed6ae47de34b34cf1d4eafd9
lzianekhodja <lzianekhodja@aneo.fr>
2022-11-07 15:33:57 +0100

    Access mongodb as admin

--- tools/access-mongo.sh
+++
@@ -1,9 +0,0 @@
-# ACESS to monogo
-MPASS=$(kubectl get secret -n armonik mongodb-user -o jsonpath="{.data.password}" | base64 --decode)
-MUSER=$(kubectl get secret -n armonik mongodb-user -o jsonpath="{.data.username}" | base64 --decode)
-kubectl get secret -n armonik mongodb-user-certificates -o jsonpath="{.data.chain\.pem}" | base64 --decode > ./mongodb_chain.pem
-MONGO_IP=$(kubectl get svc --selector="service=mongodb" -n armonik -o custom-columns="IP:.spec.clusterIP" --no-headers=true)
-docker run -it -v $(pwd)/mongodb_chain.pem:/chain.pem --rm rtsp/mongosh mongosh --tlsCAFile /chain.pem --tlsAllowInvalidCertificates --tlsAllowInvalidHostnames --tls -u $MUSER -p $MPASS mongodb://$MONGO_IP:27017/database #--eval 'db.serverStatus()'
-
-# Get the number of processed tasks by pod
-#db.TaskData.aggregate([{$group : {_id:"$OwnerPodId", count:{$sum:1}}}, {$sort: {count:1}}]).forEach(printjson);
