8925d2637a3cfac6a2e4722b3f8255cfc5a47396
lzianekhodja <lzianekhodja@aneo.fr>
2022-11-14 10:26:37 +0100

    Update version of hashicorp/random from 3.10 to 3.4.3

--- infrastructure/quick-deploy/localhost/armonik/versions.tf
+++ infrastructure/quick-deploy/localhost/armonik/versions.tf
@@ -18,7 +18,7 @@ terraform {
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.1.0"
+      version = "~> 3.4.3"
     }
     local = {
       source  = "hashicorp/local"
