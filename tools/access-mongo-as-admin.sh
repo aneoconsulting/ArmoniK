0865bb6d07eec5e5014bd89b2b31d721e6d6ffde
lzianekhodja <lzianekhodja@aneo.fr>
2022-11-28 10:59:03 +0100

    Delete commented lines and add a tool to compute the number of processed tasks per pod.

--- tools/access-mongo-as-admin.sh
+++ tools/access-mongo-as-admin.sh
@@ -4,6 +4,3 @@ MUSER=$(kubectl get secret -n armonik mongodb-admin -o jsonpath="{.data.username
 kubectl get secret -n armonik mongodb-user-certificates -o jsonpath="{.data.chain\.pem}" | base64 --decode > ./mongodb_chain.pem
 MONGO_IP=$(kubectl get svc --selector="service=mongodb" -n armonik -o custom-columns="IP:.spec.clusterIP" --no-headers=true)
 docker run -it -v $(pwd)/mongodb_chain.pem:/chain.pem --rm rtsp/mongosh mongosh --tlsCAFile /chain.pem --tlsAllowInvalidCertificates --tlsAllowInvalidHostnames --tls -u $MUSER -p $MPASS mongodb://$MONGO_IP:27017
-
-# Get the number of processed tasks by pod
-#db.TaskData.aggregate([{$group : {_id:"$OwnerPodId", count:{$sum:1}}}, {$sort: {count:1}}]).forEach(printjson);
