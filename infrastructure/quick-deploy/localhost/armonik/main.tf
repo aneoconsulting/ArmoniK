5d2b75a42831cdc783ed1fcc02c4fec17c4c81e3
Florian Lemaitre <flemaitre@aneo.fr>
2022-12-02 15:44:32 +0100

    formatting

--- infrastructure/quick-deploy/localhost/armonik/main.tf
+++ infrastructure/quick-deploy/localhost/armonik/main.tf
@@ -1,12 +1,12 @@
 module "armonik" {
-  source                     = "../../../modules/armonik"
-  working_dir                = "${path.root}/../../.."
-  namespace                  = var.namespace
-  logging_level              = var.logging_level
-  mongodb_polling_delay      = var.mongodb_polling_delay
-  storage_endpoint_url       = var.storage_endpoint_url
-  monitoring                 = var.monitoring
-  compute_plane              = { for k, v in var.compute_plane: k => merge({
+  source                = "../../../modules/armonik"
+  working_dir           = "${path.root}/../../.."
+  namespace             = var.namespace
+  logging_level         = var.logging_level
+  mongodb_polling_delay = var.mongodb_polling_delay
+  storage_endpoint_url  = var.storage_endpoint_url
+  monitoring            = var.monitoring
+  compute_plane = { for k, v in var.compute_plane : k => merge({
     partition_data = {
       priority              = 1
       reserved_pods         = 1
