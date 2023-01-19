10b608b98f105c5af4f87516827f8712243a1d38
Jérôme Gurhem <jgurhem@aneo.fr>
2023-01-19 16:26:58 +0100

    fix: increase control plane error tolerance

--- infrastructure/modules/armonik/control-plane-configmap.tf
+++ infrastructure/modules/armonik/control-plane-configmap.tf
@@ -6,5 +6,6 @@ resource "kubernetes_config_map" "control_plane_config" {
   }
   data = {
     Submitter__DefaultPartition = (local.default_partition == null || local.default_partition == "" || !contains(local.partition_names, local.default_partition) ? (length(local.partition_names) > 0 ? local.partition_names[0] : "") : local.default_partition)
+    Submitter__MaxErrorAllowed  = "50"
   }
 }
