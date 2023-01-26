bf38c0546a1ccbcb807d602f3ae7a2e9561acc71
lzianekhodja <lzianekhodja@aneo.fr>
2023-01-26 10:20:05 +0100

    display nonsensitive armonik outputs when ingress is null

--- infrastructure/modules/armonik/outputs.tf
+++ infrastructure/modules/armonik/outputs.tf
@@ -14,7 +14,6 @@ output "endpoint_urls" {
   }
 }
 
-
 output "control_plane_url" {
   description = <<-EOT
     [DEPRECATED] Endpoint URL of ArmoniK control plane
@@ -31,32 +30,4 @@ output "admin_api_url" {
 output "admin_app_url" {
   description = "Endpoint URL of ArmoniK admin App"
   value       = local.admin_app_url
-}
-
-output "ingress_DEPRECATED" {
-  description = "deprecation notice for `ingress` output"
-  value       = "`ingress` is deprecated. Please use `endpoint_urls` directly."
-}
-output "ingress" {
-  description = <<-EOT
-    [DEPRECATED] ingress endpoint
-    use endpoint_urls instead
-  EOT
-  value = var.ingress != null ? {
-    http              = local.ingress_http_url
-    grpc              = local.ingress_grpc_url
-    control_plane_url = local.ingress_grpc_url != "" ? local.ingress_grpc_url : local.control_plane_url
-    grafana_url       = data.kubernetes_secret.grafana.data.enabled ? (local.ingress_http_url != "" ? "${local.ingress_http_url}/grafana/" : data.kubernetes_secret.grafana.data.url) : ""
-    seq_web_url       = data.kubernetes_secret.seq.data.enabled ? (local.ingress_http_url != "" ? "${local.ingress_http_url}/seq/" : data.kubernetes_secret.seq.data.web_url) : ""
-    admin_api_url     = local.ingress_http_url != "" ? "${local.ingress_http_url}/api" : local.admin_api_url
-    admin_app_url     = local.ingress_http_url != "" ? "${local.ingress_http_url}/" : local.admin_app_url
-    } : {
-    http              = ""
-    grpc              = ""
-    control_plane_url = local.control_plane_url
-    grafana_url       = data.kubernetes_secret.grafana.data.url
-    seq_web_url       = data.kubernetes_secret.seq.data.web_url
-    admin_api_url     = local.admin_api_url
-    admin_app_url     = local.admin_app_url
-  }
 }
