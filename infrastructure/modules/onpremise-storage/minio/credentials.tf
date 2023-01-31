258dd472306f5dd475a41d18256a6a2a220b20a9
lzianekhodja <lzianekhodja@aneo.fr>
2023-01-31 10:44:56 +0100

    Merge with main26

---
+++ infrastructure/modules/onpremise-storage/minio/credentials.tf
@@ -0,0 +1,26 @@
+resource "random_string" "minio_application_user" {
+  length  = 8
+  special = false
+  numeric = false
+}
+
+resource "random_password" "minio_application_password" {
+  length  = 16
+  special = false
+}
+
+resource "kubernetes_secret" "s3_endpoints" {
+  metadata {
+    name      = "s3-endpoints"
+    namespace = var.namespace
+  }
+  data = {
+    url                   = "http://${var.minio.host}:${local.port}"
+    host                  = var.minio.host
+    port                  = local.port
+    login                 = random_string.minio_application_user.result
+    password              = random_password.minio_application_password.result
+    bucket_name           = var.minio.bucket_name
+    must_force_path_style = true
+  }
+}
