ef169fea579c2fe52f8c72d73513421907daa453
alouvion-aneo <113617172+alouvion-aneo@users.noreply.github.com>
2023-02-10 10:48:24 +0100

    update display

--- tools/access-mongo-aws-as-admin.sh
+++ tools/access-mongo-aws-as-admin.sh
@@ -3,11 +3,21 @@
 set -e
 
 cat <<EOF
-***** this script allows you to connect to mongo directly from inside the cluster => useful for AWS installation *****
-Firstly you have to connect to db : use database
-Then you can try requests ex :
-Display all TaskData : db.TaskData.find().pretty()
-db.TaskData.find({ SessionId: { \$eq : '7eafe4e3-0aa2-46ef-8ce6-bf9e365c5449' }, ExpectedOutputIds: { \$eq : 'a600dca5-b672-4177-9b4a-880dbcefee4e'}}).pretty()
+**********************************************************************************************************************
+***** This script allows you to connect to mongo directly from inside the cluster => useful for AWS installation *****
+**********************************************************************************************************************
+
+1 - Firstly you have to connect to db :
+  use database
+
+2 - You can execute requests ex :
+- Display all TaskData :
+  db.TaskData.find().limit(3).pretty()
+- Filter by  session / output :
+  db.TaskData.find({ SessionId: { \$eq : '7eafe4e3-0aa2-46ef-8ce6-bf9e365c5449' }, ExpectedOutputIds: { \$eq : 'a600dca5-b672-4177-9b4a-880dbcefee4e'}}).pretty()
+
+more informations here : https://www.mongodb.com/docs/manual/reference/method/db.collection.find/
+
 EOF
 
 kubectl replace --force -f mongo_client_in_k8s.yaml
