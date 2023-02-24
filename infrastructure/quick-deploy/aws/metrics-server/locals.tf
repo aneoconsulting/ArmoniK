fa164eb739a9165aa0aaef451ab7a5aeb1643b22
Yanis KHAROUNI <ykharouni@aneo.fr>
2023-02-24 18:08:22 +0100

    Use coalesce on chart helm repository and chart helm version

--- infrastructure/quick-deploy/aws/metrics-server/locals.tf
+++ infrastructure/quick-deploy/aws/metrics-server/locals.tf
@@ -12,6 +12,6 @@ locals {
     "--metric-resolution=15s"
   ] : var.args
   host_network          = try(var.host_network, false)
-  helm_chart_repository = try(var.helm_chart_repository, "https://kubernetes-sigs.github.io/metrics-server/")
-  helm_chart_version    = try(var.helm_chart_version, "3.8.3")
+  helm_chart_repository = try(coalesce(var.helm_chart_repository), "https://kubernetes-sigs.github.io/metrics-server/")
+  helm_chart_version    = try(coalesce(var.helm_chart_version), "3.8.3")
 }
