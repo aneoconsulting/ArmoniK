56d73bfcf61525af73df874a1dd51ac62566cc8e
Faust1-2was-Aneo <fdewas@aneo.fr>
2023-02-27 14:15:44 +0100

    Created old gui tf file
    It contains the old app and the needed api, since they are dependent from each other.
    
    The new app doesn't require the api anymore.

--- infrastructure/modules/armonik/admin-gui.tf
+++ infrastructure/modules/armonik/admin-gui.tf
@@ -48,64 +48,7 @@ resource "kubernetes_deployment" "admin_gui" {
           }
         }
         restart_policy = "Always" # Always, OnFailure, Never
-        # API container
-        container {
-          name              = var.admin_gui.api.name
-          image             = var.admin_gui.api.tag != "" ? "${var.admin_gui.api.image}:${var.admin_gui.api.tag}" : var.admin_gui.api.image
-          image_pull_policy = var.admin_gui.image_pull_policy
-          resources {
-            limits   = var.admin_gui.api.limits
-            requests = var.admin_gui.api.requests
-          }
-          port {
-            name           = "api-port"
-            container_port = 3333
-          }
-          env_from {
-            config_map_ref {
-              name = kubernetes_config_map.core_config.metadata.0.name
-            }
-          }
-          env {
-            name  = "ControlPlane__Endpoint"
-            value = local.control_plane_url
-          }
-          dynamic "env" {
-            for_each = (data.kubernetes_secret.grafana.data.enabled ? [1] : [])
-            content {
-              name  = "Grafana__Endpoint"
-              value = data.kubernetes_secret.grafana.data.url
-            }
-          }
-          dynamic "env" {
-            for_each = (data.kubernetes_secret.seq.data.enabled ? [1] : [])
-            content {
-              name  = "Seq__Endpoint"
-              value = data.kubernetes_secret.seq.data.web_url
-            }
-          }
-          dynamic "env" {
-            for_each = local.credentials
-            content {
-              name = env.key
-              value_from {
-                secret_key_ref {
-                  key      = env.value.key
-                  name     = env.value.name
-                  optional = false
-                }
-              }
-            }
-          }
-          dynamic "volume_mount" {
-            for_each = local.certificates
-            content {
-              name       = volume_mount.value.name
-              mount_path = volume_mount.value.mount_path
-              read_only  = true
-            }
-          }
-        }
+         
         # App container
         container {
           name              = var.admin_gui.app.name
@@ -175,65 +118,6 @@ resource "kubernetes_deployment" "admin_gui" {
             }
           }
         }
-
-        # Old GUI container
-        container {
-          name              = var.admin_gui.old.name
-          image             = var.admin_gui.old.tag != "" ? "${var.admin_gui.old.image}:${var.admin_gui.old.tag}" : var.admin_gui.old.image
-          image_pull_policy = var.admin_gui.image_pull_policy
-          resources {
-            limits   = var.admin_gui.old.limits
-            requests = var.admin_gui.old.requests
-          }
-          port {
-            name           = "old-port"
-            container_port = 1081
-          }
-          env_from {
-            config_map_ref {
-              name = kubernetes_config_map.core_config.metadata.0.name
-            }
-          }
-          env {
-            name  = "ControlPlane__Endpoint"
-            value = local.control_plane_url
-          }
-          dynamic "env" {
-            for_each = (data.kubernetes_secret.grafana.data.enabled != "" ? [1] : [])
-            content {
-              name  = "Grafana__Endpoint"
-              value = data.kubernetes_secret.grafana.data.url
-            }
-          }
-          dynamic "env" {
-            for_each = (data.kubernetes_secret.seq.data.enabled ? [1] : [])
-            content {
-              name  = "Seq__Endpoint"
-              value = data.kubernetes_secret.seq.data.web_url
-            }
-          }
-          dynamic "env" {
-            for_each = local.credentials
-            content {
-              name = env.key
-              value_from {
-                secret_key_ref {
-                  key      = env.value.key
-                  name     = env.value.name
-                  optional = false
-                }
-              }
-            }
-          }
-          dynamic "volume_mount" {
-            for_each = local.certificates
-            content {
-              name       = volume_mount.value.name
-              mount_path = volume_mount.value.mount_path
-              read_only  = true
-            }
-          }
-        }
       }
     }
   }
@@ -257,21 +141,9 @@ resource "kubernetes_service" "admin_gui" {
       service = kubernetes_deployment.admin_gui[0].metadata.0.labels.service
     }
     port {
-      name        = kubernetes_deployment.admin_gui[0].spec.0.template.0.spec.0.container.0.port.0.name
-      port        = var.admin_gui.api.port
-      target_port = kubernetes_deployment.admin_gui[0].spec.0.template.0.spec.0.container.0.port.0.container_port
-      protocol    = "TCP"
-    }
-    port {
       name        = kubernetes_deployment.admin_gui[0].spec.0.template.0.spec.0.container.1.port.0.name
       port        = var.admin_gui.app.port
       target_port = kubernetes_deployment.admin_gui[0].spec.0.template.0.spec.0.container.1.port.0.container_port
-      protocol    = "TCP"
-    }
-    port {
-      name        = kubernetes_deployment.admin_gui[0].spec.0.template.0.spec.0.container.2.port.0.name
-      port        = var.admin_gui.old.port
-      target_port = kubernetes_deployment.admin_gui[0].spec.0.template.0.spec.0.container.2.port.0.container_port
       protocol    = "TCP"
     }
   }
