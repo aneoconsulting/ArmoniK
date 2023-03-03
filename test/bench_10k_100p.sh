cfbd148ddc34e9682f03aeecd9dbde6642b93f01
Yanis KHAROUNI <ykharouni@aneo.fr>
2023-03-03 10:55:44 +0100

    set the bench script for 10k tasks on 100 pods

--- test/bench_10k_100p.sh
+++ test/bench_10k_100p.sh
@@ -8,11 +8,11 @@ export CONTROL_PLANE_URL=http://a24fc0af6e62547d6b9b26d86873fd36-1938019023.eu-w
 #run  10000 tasks on 100 pods
 docker run --rm \
 	-e GrpcClient__Endpoint="${CONTROL_PLANE_URL}" \
-	-e BenchOptions__nTasks=100 \
+	-e BenchOptions__nTasks=10000 \
 	-e BenchOptions__TaskDurationMs=10 \
 	-e BenchOptions__PaloadSize=100 \
 	-e BenchOptions__ResultSize=100 \
 	-e BenchOptions__Partition=default \
     -e BenchOptions__ShowEvents=true \
-	-e BenchOptions__BatchSize=100 \
+	-e BenchOptions__BatchSize=1000 \
 	dockerhubaneo/armonik_core_bench_test_client:0.11.2-jgfixbench.8.be5c1433
