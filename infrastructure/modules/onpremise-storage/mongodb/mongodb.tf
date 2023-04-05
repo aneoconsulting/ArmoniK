86887ed25a4d12034fde3395db2e7c447853f261
melflitty <melflitty@aneo.fr>
2023-04-05 07:22:23 +0000

    fix: fix circular dependencies issues

--- infrastructure/modules/onpremise-storage/mongodb/mongodb.tf
+++ infrastructure/modules/onpremise-storage/mongodb/mongodb.tf
@@ -144,8 +144,8 @@ resource "kubernetes_deployment" "mongodb" {
 resource "kubernetes_service" "mongodb" {
   for_each = local.replicas
   metadata {
-    name      = kubernetes_deployment.mongodb[each.key].metadata.0.name
-    namespace = kubernetes_deployment.mongodb[each.key].metadata.0.namespace
+    name = "mongodb-${each.key}"
+    namespace = var.namespace
     labels = {
       app     = "storage"
       type    = "table"
