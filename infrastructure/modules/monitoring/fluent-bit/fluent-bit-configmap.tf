911d593f4f94c8f6a38dea1885109c8ef25ab746
lzianekhodja <lzianekhodja@aneo.fr>
2023-04-11 08:38:55 +0200

    Use an envvars for th eparser type

--- infrastructure/modules/monitoring/fluent-bit/fluent-bit-configmap.tf
+++ infrastructure/modules/monitoring/fluent-bit/fluent-bit-configmap.tf
@@ -13,7 +13,7 @@ resource "kubernetes_config_map" "fluent_bit_config" {
   }
   data = {
     "fluent-bit.conf"        = file("${path.module}/configs/service/fluent-bit.conf")
-    "input-kubernetes.conf"  = (local.fluent_bit_is_daemonset ? file("${path.module}/configs/input/input-kubernetes-daemonset-${local.fluent_bit_parser}.conf") : file("${path.module}/configs/input/input-kubernetes-sidecar.conf"))
+    "input-kubernetes.conf"  = (local.fluent_bit_is_daemonset ? file("${path.module}/configs/input/input-kubernetes-daemonset.conf") : file("${path.module}/configs/input/input-kubernetes-sidecar.conf"))
     "filter-kubernetes.conf" = (local.fluent_bit_is_daemonset ? file("${path.module}/configs/filter/filter-kubernetes-daemonset.conf") : file("${path.module}/configs/filter/filter-kubernetes-sidecar.conf"))
     "output-http-seq.conf"   = (local.seq_enabled ? file("${path.module}/configs/output/output-http-seq.conf") : file("${path.module}/configs/output/output-empty.conf"))
     "output-cloudwatch.conf" = (local.cloudwatch_enabled ? file("${path.module}/configs/output/output-cloudwatch.conf") : file("${path.module}/configs/output/output-empty.conf"))
