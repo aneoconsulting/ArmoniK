5789b67a8f9649813ba525404f97dda39a5e0458
Estéban <103997512+esoubiran-aneo@users.noreply.github.com>
2023-04-24 13:40:04 +0200

    Update infrastructure/utils/scripts/installation/prerequisites/install-k3s.sh
    Co-authored-by: Dylan Brasseur <103190315+dbrasseur-aneo@users.noreply.github.com>

--- infrastructure/utils/scripts/installation/prerequisites/install-k3s.sh
+++ infrastructure/utils/scripts/installation/prerequisites/install-k3s.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script is used to install k3s.
 
-curl -sfL https://get.k3s.io |sh -s - --write-kubeconfig-mode 644 --docker --write-kubeconfig ~/.kube/config
+curl -sfL https://get.k3s.io |sh -s - --write-kubeconfig-mode 644 --docker --write-kubeconfig ~/.kube/config --kubelet-arg cgroup-driver=systemd
 
 # To uninstall
 # sudo /usr/local/bin/k3s-uninstall.sh
