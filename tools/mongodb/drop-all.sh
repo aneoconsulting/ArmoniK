7dfd84a52781c8703506983c161ddae0a0b206d8
esoubiran-aneo <esoubiran@aneo.fr>
2023-05-29 15:07:05 +0200

    chore: remove drop scripts

--- tools/mongodb/drop-all.sh
+++
@@ -1,2 +0,0 @@
-# Description: Drop all collections from MongoDB
-$(pwd)/tools/mongodb/utils/execute-script.sh drop-all
--- tools/mongodb/scripts/drop-all.js
+++
@@ -1,7 +0,0 @@
-db = db.getSiblingDB("database");
-
-db.getCollectionNames().forEach(function (collection) {
-  if (collection != "system.indexes") {
-    db.getCollection(collection).drop();
-  }
-});
