261352461f3c97a9b1372a1b260f6ff6b8d2b18f
esoubiran-aneo <esoubiran@aneo.fr>
2023-05-29 14:58:19 +0200

    chore: todo

--- tools/mongodb/scripts/generate-partitions.js
+++ tools/mongodb/scripts/generate-partitions.js
@@ -3,15 +3,13 @@ const faker = localRequire("@faker-js/faker").fakerEN;
 
 db = db.getSiblingDB("database");
 
-// TODO: clean the database in an another script (and please, don't forget to document it) and add some sh script to run all the scripts in the correct order
-
-// Create a new item in the collection named "PartitionData". The collection is already created in the database.
 // Create 100 partitions
 const partitionsNumber = 100;
 
 for (let i = 0; i < partitionsNumber; i++) {
   db.PartitionData.insertOne({
     _id: faker.string.uuid(),
+    // TODO: Which value can be used here?
     ParentPartitionIds: [],
     // TODO: Which value can be used here?
     PodConfiguration: null,
