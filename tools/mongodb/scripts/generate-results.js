440226d3b11af0f9a98a50da77157e9f71a11328
esoubiran-aneo <esoubiran@aneo.fr>
2023-06-23 16:17:16 +0200

    fix: results data structure

--- tools/mongodb/scripts/generate-results.js
+++ tools/mongodb/scripts/generate-results.js
@@ -7,18 +7,26 @@ db = db.getSiblingDB("database");
 const resultsNumber = 100;
 
 for (let i = 0; i < resultsNumber; i++) {
-  const createdAt = faker.date.past();
-  const completedAt = faker.date.future({ refDate: createdAt });
+  const creationDate = faker.date.past();
+  const completionDate = faker.date.future({ refDate: creationDate });
   db.Result.insertOne({
     SessionId: faker.string.uuid(),
     Name: faker.word.sample(),
     OwnerTaskId: faker.string.uuid(),
-    status: faker.number.int({
+    Status: faker.number.int({
       min: 0,
       max: 3
     }),
-    CreatedAt: createdAt,
-    CompletedAt: completedAt,
-    ResultId: new ObjectId(),
+    DependentTasks: Array.from({
+      length: faker.number.int({
+        min: 1,
+        max: 10
+      })
+    }, () => faker.string.uuid()),
+    // Binary data
+    Data: faker.number.octal(),
+    CreationDate: creationDate,
+    CompletionDate: completionDate,
+    _id: faker.string.uuid(),
   });
 }
