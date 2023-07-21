566b37397ab24166cb4533095ff06d432196ac9d
lzianekhodja <lzianekhodja@aneo.fr>
2023-07-21 18:43:24 +0200

    delete external data to get k8s context

--- infrastructure/quick-deploy/localhost/all-in-one/providers.tf
+++ infrastructure/quick-deploy/localhost/all-in-one/providers.tf
@@ -1,12 +1,6 @@
-# K8s configuration
-data "external" "k8s_config_context" {
-  program     = ["bash", "k8s_config.sh", var.k8s_config_path]
-  working_dir = ".${path.root}/../../utils/scripts"
-}
-
 provider "kubernetes" {
   config_path    = var.k8s_config_path
-  config_context = lookup(tomap(data.external.k8s_config_context.result), "k8s_config_context", var.k8s_config_context)
+  config_context = var.k8s_config_context
 }
 
 # package manager for kubernetes
@@ -14,6 +8,6 @@ provider "helm" {
   helm_driver = "configmap"
   kubernetes {
     config_path    = var.k8s_config_path
-    config_context = lookup(tomap(data.external.k8s_config_context.result), "k8s_config_context", var.k8s_config_context)
+    config_context = var.k8s_config_context
   }
 }
