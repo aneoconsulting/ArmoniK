690e1695a9def039b0ed978b194dc043bc58f52d
Florian Lemaitre <flemaitre@aneo.fr>
2024-02-26 11:09:08 +0100

    Add nodeSelector

--- infrastructure/modules/k8s/chaos_mesh/main.tf
+++ infrastructure/modules/k8s/chaos_mesh/main.tf
@@ -12,6 +12,24 @@ resource "helm_release" "chaosmesh" {
   repository = var.helm_chart_repository
   version    = var.helm_chart_version
 
+  values = var.node_selector != null ? [yamlencode({
+    controllerManager = {
+      nodeSelector = var.node_selector
+    }
+    chaosDaemon = {
+      nodeSelector = var.node_selector
+    }
+    dashboard = {
+      nodeSelector = var.node_selector
+    }
+    dnsServer = {
+      nodeSelector = var.node_selector
+    }
+    prometheus = {
+      nodeSelector = var.node_selector
+    }
+  })] : []
+
   set {
     name  = "image.chaosmesh.repository"
     value = var.docker_image.chaosmesh.image
@@ -29,36 +47,6 @@ resource "helm_release" "chaosmesh" {
     name  = "dashboard.service.type"
     value = var.service_type
   }
-
-  # commented coz it crash with the error : 
-  #                        var.node_selector is object with no attributes
-  #                        Inappropriate value for attribute "value": string required
-
-  #set {
-  #  name  = "controllerManager.nodeSelector"
-  #  value = var.node_selector
-  #}
-
-  #set {
-  #  name  = "chaosDaemon.nodeSelector"
-  #  value = var.node_selector
-  #}
-
-  #set {
-  #  name  = "dashboard.nodeSelector"
-  #  value = var.node_selector
-  #}
-
-  #set {
-  #  name  = "dnsServer.nodeSelector"
-  #  value = var.node_selector
-  #}
-
-  #set {
-  #  name  = "prometheus.nodeSelector"
-  #  value = var.node_selector
-  #}
-
 }
 
 data "kubernetes_service" "chaos_dashboard" {
