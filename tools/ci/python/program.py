36cca9cdfd3a8b4d79008138df1539afbd05cd94
Trystan Schneider <tschneider@aneo.fr>
2024-10-17 18:12:08 +0200

    Update get throughput program to dump json

--- tools/ci/python/program.py
+++ tools/ci/python/program.py
@@ -2,6 +2,9 @@ import datetime
 import time
 import grpc
 import argparse
+import json
+import os
+from pathlib import Path
 from armonik.client import ArmoniKTasks, ArmoniKSessions
 from armonik.common import Task, TaskStatus, Session, SessionStatus, Direction
 
@@ -13,7 +16,7 @@ def get_session_id_by_name(session_name: str, grpc_channel) -> str:
     )[1][-1].session_id
 
 
-def get_session_throughput(session_id: str, grpc_channel: grpc.Channel):
+def get_session_stats(session_id: str, grpc_channel: grpc.Channel) -> dict:
     tasks_client = ArmoniKTasks(grpc_channel)
 
     class ThroughputGetterHelper:
@@ -44,7 +47,11 @@ def get_session_throughput(session_id: str, grpc_channel: grpc.Channel):
         f"Nb of tasks: {tasks_nb}, First task started at: {first_task.started_at}, Last task ended at: {last_task.ended_at}"
     )
 
-    return tasks_nb / (last_task.ended_at - first_task.started_at).total_seconds()
+    return {
+        "tasks_nb": tasks_nb,
+        "throughput": tasks_nb
+        / (last_task.ended_at - first_task.started_at).total_seconds(),
+    }
 
 
 def poll_session_ending(
@@ -69,28 +76,58 @@ def poll_session_ending(
     print("Timeout date has been exceeded.")
 
 
-def main(session_name: str, grpc_endpoint: str, polling_limit: float):
+def main(session_name: str, grpc_endpoint: str, polling_limit: float, path: str):
     try:
         with grpc.insecure_channel(f"{grpc_endpoint}:5001") as channel:
             session_id = get_session_id_by_name(session_name, channel)
             poll_session_ending(session_id, channel, polling_limit)
-            throughput = get_session_throughput(session_id, channel)
-            print(
-                f"Throughput for session named '{session_name}' with id {session_id}: {throughput} Tasks per second"
-            )
+            session_stats = get_session_stats(session_id, channel)
+
     except:  # noqa: E722
         print(
             f"Session {session_name} was not found or gRPC channel located at {grpc_endpoint} cannot be reached."
         )
+        return
+
+    print(
+        f"Throughput for session named '{session_name}' with id {session_id}: {session_stats['throughput']} Tasks per second"
+    )
+
+    write_json_output(session_id, session_stats, path)
+
+
+def write_json_output(session_id: str, session_stats: dict, path: str = ""):
+    json_bench = [
+        {
+            "name": "HTC Mock Throughput",
+            "unit": "Task per second",
+            "value": session_stats["throughput"],
+        }
+    ]
+
+    json_string = json.dumps(json_bench, indent=2)
+
+    print(f"JSON output written in benchmark.json:\n {json_string}")
+
+    jsonfile_directory = Path(os.getcwd(), path)
+    jsonfile_directory.mkdir(parents=True, exist_ok=True)
+    jsonfile_name = (
+        f"session_{session_id}_benchmark_{session_stats['tasks_nb']}tasks.json"
+    )
+    jsonfile_path = jsonfile_directory / jsonfile_name
+
+    with open(jsonfile_path, "w") as output_file:
+        output_file.write(json_string)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("grpc_endpoint", type=str)
-    parser.add_argument("session_name", type=str)
+    parser.add_argument("-n", "--session-name", type=str, default="")
     parser.add_argument("--polling-limit", type=float, default=300)
+    parser.add_argument("-p", "--output-path", type=str, default="")
 
     args = parser.parse_args()
 
-    main(args.session_name, args.grpc_endpoint, args.polling_limit)
+    main(args.session_name, args.grpc_endpoint, args.polling_limit, args.output_path)
