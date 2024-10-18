7eecf72c314111812e81e489631bda7dbe93dd0e
github-action-benchmark <github@users.noreply.github.com>
2024-10-18 15:54:11 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 141a828ebba2c2d9632691e82c47292ff437899c

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1729266851157,
+  "repoUrl": "https://github.com/aneoconsulting/ArmoniK",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "tschneider@aneo.fr",
+            "name": "Trystan Schneider",
+            "username": "tschneider-aneo"
+          },
+          "committer": {
+            "email": "tschneider@aneo.fr",
+            "name": "Trystan Schneider",
+            "username": "tschneider-aneo"
+          },
+          "distinct": true,
+          "id": "141a828ebba2c2d9632691e82c47292ff437899c",
+          "message": "Change gh pages to upload-bench-results in benchmark action",
+          "timestamp": "2024-10-18T17:50:49+02:00",
+          "tree_id": "0d1b4d3d488f26b4e4a11dae01af102b8c28a781",
+          "url": "https://github.com/aneoconsulting/ArmoniK/commit/141a828ebba2c2d9632691e82c47292ff437899c"
+        },
+        "date": 1729266847889,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "HTC Mock Throughput",
+            "value": 29.369384641464656,
+            "unit": "Task per second"
+          }
+        ]
+      }
+    ]
+  }
+}
