ee09cf2b36bb0c157669fae67fd01b31c5c0f610
github-action-benchmark <github@users.noreply.github.com>
2024-10-21 09:43:16 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 1724fa5c0178a94ec1f346fd94d595a8eb577ab8

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1729503796403,
+  "repoUrl": "https://github.com/aneoconsulting/ArmoniK",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "tschneider@aneo.fr",
+            "name": "Trystan Schneider",
+            "username": "tschneider-aneo"
+          },
+          "committer": {
+            "email": "tschneider@aneo.fr",
+            "name": "Trystan Schneider",
+            "username": "tschneider-aneo"
+          },
+          "distinct": true,
+          "id": "1724fa5c0178a94ec1f346fd94d595a8eb577ab8",
+          "message": "Remove core-version input from htcmock localhost workflow",
+          "timestamp": "2024-10-21T11:40:20+02:00",
+          "tree_id": "d031e43929f1753f40c4b7cdd5624cec9f8180d2",
+          "url": "https://github.com/aneoconsulting/ArmoniK/commit/1724fa5c0178a94ec1f346fd94d595a8eb577ab8"
+        },
+        "date": 1729503796110,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "HTC Mock Throughput",
+            "value": 23.990860624523993,
+            "unit": "Task per second"
+          }
+        ]
+      }
+    ]
+  }
+}
