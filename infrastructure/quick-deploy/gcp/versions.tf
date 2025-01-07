e36490ad24448a07db2e224f2099bf2540baca33
Mohamed Khairallah Gharbi <mgharbi@aneo.fr>
2025-01-07 17:29:27 +0100

    update provider version gcp

--- infrastructure/quick-deploy/gcp/versions.tf
+++ infrastructure/quick-deploy/gcp/versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.78.0"
+      version = "> 4.78.0"
     }
     random = {
       source  = "hashicorp/random"
