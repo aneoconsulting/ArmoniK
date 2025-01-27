0a6428198a09233e9b7d9ca43797f225f9fb1cd6
Mohamed Khairallah Gharbi <mgharbi@aneo.fr>
2025-01-27 15:30:34 +0100

    customize active mq in gcp

--- infrastructure/quick-deploy/gcp/variables.tf
+++ infrastructure/quick-deploy/gcp/variables.tf
@@ -610,6 +610,15 @@ variable "activemq" {
     image_tag          = optional(string)
     node_selector      = optional(any, {})
     image_pull_secrets = optional(string, "")
+    limits = optional(object({
+      cpu    = optional(string)
+      memory = optional(string)
+    }))
+    requests = optional(object({
+      cpu    = optional(string)
+      memory = optional(string)
+    }))
+    activemq_opts_memory = optional(string, "-Xms1g -Xmx3g")
   })
   default = null
 }
