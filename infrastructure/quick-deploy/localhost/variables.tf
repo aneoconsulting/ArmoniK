823cf079ec8ff9dab6df081d1605dff520ef291f
sleep <abde.arrahman.dk@gmail.com>
2025-07-18 09:31:08 +0200

    Added the mongodb exporter to GCP as well as adjustments to aws/localhost

--- infrastructure/quick-deploy/localhost/variables.tf
+++ infrastructure/quick-deploy/localhost/variables.tf
@@ -405,9 +405,9 @@ variable "partition_metrics_exporter" {
 variable "mongodb_metrics_exporter" {
   description = "MongoDB Metrics Exporter configuration (nullable)"
   type = object({
-    image_name         = optional(string, "percona/mongodb_exporter")
-    image_tag          = optional(string)
-    image_pull_secrets = optional(string)
+    image_name   = optional(string, "percona/mongodb_exporter")
+    image_tag    = optional(string)
+    pull_secrets = optional(string, "")
   })
   default = null
 }
