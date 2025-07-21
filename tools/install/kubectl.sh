2921d49001c5fa329e62471d7b1de8c68e85cb5f
José Fonseca <jfonseca@aneo.fr>
2025-07-21 11:26:10 +0200

    chore: remove duplicated scripts

--- tools/install/kubectl.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-set -ex
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-echo "$(cat kubectl.sha256) kubectl" | sha256sum --check
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
