8f861c90a9943dd74c97d2f48d931923a80ec773
José Fonseca <jfonseca@aneo.fr>
2025-07-24 12:24:28 +0200

    purge more unused scripts

--- tools/mongodb/archived/generate-real-world-run.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
-# Description: Generate results for ArmoniK
-"$DIR/utils/execute-mongo-shell-script.sh" generate-real-world-run
