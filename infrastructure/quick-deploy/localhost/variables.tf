c1783d0a831441f4ddcd7d4bc3c5b5108df33ec9
Faustin <fdewas@aneo.fr>
2026-01-19 10:13:29 +0100

    chore: poc sso

--- infrastructure/quick-deploy/localhost/variables.tf
+++ infrastructure/quick-deploy/localhost/variables.tf
@@ -669,3 +669,14 @@ variable "static" {
   type        = any
   default     = {}
 }
+
+variable "oauth_configuration" {
+  type = object({
+    provider_root_URI               = string
+    provider_user_info_endpoint     = string
+    provider_authorization_endpoint = string
+    client_id                       = string
+    response_type                   = optional(string, "refresh_token")
+  })
+  default = null
+}
